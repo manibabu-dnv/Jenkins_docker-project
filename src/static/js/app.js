3d59c82aa5ea1e5a475f310aba44fe2497af3c3e
manibabu-dnv <d.n.v.manibabu@gmail.com>
2025-04-30 13:19:06 +0530

    latestcodee

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'ADD uday'}
+                        {submitting ? 'Adding...' : 'ADD ITEM'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
